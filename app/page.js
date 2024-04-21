51efdda6cf3f22e182645f5527eb82de4bef82ae
vimarshmishra37 <144466200+vimarshmishra37@users.noreply.github.com>
2024-04-21 22:59:14 +0530

    signup-signin

--- app/page.js
+++ app/page.js
@@ -1,19 +1,69 @@
-import React from 'react'; // Import React
-// You don't need to import Image if you're not using it
+
+'use client'
+import React, { useState } from 'react';
+import './globals.css';
+import Navbar from './routes/Navbar';
 
 export default function Home() {
-  return (
-    <div>
-      <form action="http://localhost:3000/register" method="post">
+  const [isSignUpActive, setIsSignUpActive] = useState(false);
 
-        <input type="text" name="firstname" id="firstname"/>
-        <input type="text" name="lastname" id="lastname"/>
-        <input type="text" name="username" id="username"/>
-         <input type="text" name="email" id="email"/>
-         <input type="text" name="password" id="password"/>
-         <input type="submit" value="Submit"/>    
-  
-      </form>
+  const handleSignUpClick = () => {
+    setIsSignUpActive(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUpActive(false);
+  };
+
+  return (
+    <div className={`container ${isSignUpActive ? 'right-panel-active' : ''}`}>
+      <div className={`form-container ${isSignUpActive ? 'sign-up-container' : 'sign-in-container'}`}>
+        {isSignUpActive ? (
+          <form action="#">
+            <h1>Create Account</h1>
+            <div className="social-container">
+              <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+              <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+              <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
+        ) : (
+          <form action="#">
+            <div>
+              <h1>Sign in</h1>
+              <div className="social-container">
+                <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+              </div>
+              <span>or use your account</span>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <a href="#">Forgot your password?</a>
+              <button>Sign In</button>
+            </div>
+          </form>
+        )}
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us please login with your personal info</p>
+            <button className="ghost" onClick={handleSignInClick}>Sign In</button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button className="ghost" onClick={handleSignUpClick}>Sign Up</button>
+          </div>
+        </div>
+      </div>
     </div>
-  );b
+  );
 }
